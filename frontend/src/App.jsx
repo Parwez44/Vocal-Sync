2db38a2968deedf6d0a10af8e257d54cecdf3104
Rudra-Patel-766 <rudrapatel762006@gmail.com>
2026-03-02 18:20:21 +0530

    Added Frontend folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { HomePage } from './pages/HomePage'
+import { Routes, Route } from 'react-router';
+
+function App() {
+  return (
+    <Routes>
+      <Route index element={<HomePage />} />
+    </Routes>
+  )
+}
+
+export default App
